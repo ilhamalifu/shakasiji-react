6e90a239efcd64fde2bba73cb4917362cdcf4ece
Ilham Alif Furqon <ilhamalip@gmail.com>
2026-01-10 05:45:35 +0700

    Add config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Untuk GitHub Pages, ganti 'shakasiji-react' dengan nama repo Anda
-  // Hapus baris base jika deploy ke Vercel/Netlify
+  // Untuk GitHub Pages: ganti dengan nama repo Anda
+  // Untuk Vercel/Netlify: hapus atau comment baris base
   base: '/shakasiji-react/',
 })
